fc21cb14f4d23d79fc7ff64fefec645a894a170f
SudipR <lycansu@gmail.com>
2023-04-26 20:36:21 +0545

    Typo fix

--- resources/views/frontend/pages/jobs.blade.php
+++ resources/views/frontend/pages/jobs.blade.php
@@ -75,7 +75,7 @@
       </div>
       <div class="col-lg-9 col-md-8 col-sm-12 col-xs-12">
         <div class="__job_list_title">
-          <h5>Lasted jobs</h5>
+          <h5>Latest jobs</h5>
         </div>
 
         <div id="ajax-job-list">
