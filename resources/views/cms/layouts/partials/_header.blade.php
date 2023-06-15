a575ca6ca71e75750f86a225b5d1cce96362256b
SudipR <lycansu@gmail.com>
2023-06-15 16:18:38 +0545

    Notifications off

--- resources/views/cms/layouts/partials/_header.blade.php
+++ resources/views/cms/layouts/partials/_header.blade.php
@@ -74,7 +74,8 @@
         </div>
       </li> --}}
       <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
+
+      {{-- <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
           <i class="far fa-bell"></i>
           <span class="badge badge-warning navbar-badge">12</span>
@@ -99,7 +100,7 @@
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
         </div>
-      </li>
+      </li> --}}
       <li class="nav-item">
         <a class="nav-link __lights_toggle" href="javascript:void(0);">
             <i class="fa fa-solid fa-moon"></i>
